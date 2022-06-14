ddb11e1871d6b6ce2fc99b6e703006859702862e
Katie Harshman <katie@ibm.com>
2022-06-14 08:47:00 -0700

    fix module1

--- ssg/site.py
+++ ssg/site.py
@@ -8,13 +8,12 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + "/" + path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-
         for path in self.source.rglob("*"):
-            if path is Directory:
+            if path.is_dir():
                 self.create_dir(path)
             
